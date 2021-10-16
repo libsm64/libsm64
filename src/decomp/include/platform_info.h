19dfbedc2061d37b8eb8d91dad0a9a859ebaa58a
ficool2 <34815548+ficool2@users.noreply.github.com>
2021-10-16 22:34:26 +0100

    Fix endian order on MSVC

--- src/decomp/include/platform_info.h
+++ src/decomp/include/platform_info.h
@@ -7,7 +7,11 @@
 #else
 #include <stdint.h>
 #define IS_64_BIT (UINTPTR_MAX == 0xFFFFFFFFFFFFFFFFU)
+#ifdef _MSC_VER
+#define IS_BIG_ENDIAN 0
+#else
 #define IS_BIG_ENDIAN (__BYTE_ORDER__ == __ORDER_BIG_ENDIAN__)
+#endif
 #endif
 
 #define DOUBLE_SIZE_ON_64_BIT(size) ((size) * (sizeof(void *) / 4))
