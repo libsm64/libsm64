43cc7c97009450de86ea4a4ce9e36ec83adda78b
MeltyPlayer <mastergamepro@hotmail.com>
2022-10-21 00:15:13 -0500

    Pulled in the audio code from @ckosmic's fork, which is from the decomp project.

---
+++ src/decomp/audio/load_dat.c
@@ -0,0 +1,11 @@
+#include "load_dat.h"
+
+unsigned char* gSoundDataADSR;
+
+unsigned char* gSoundDataRaw;
+
+unsigned char* gMusicData;
+
+#ifndef VERSION_SH
+unsigned char* gBankSetsData;
+#endif
---
+++ src/decomp/audio/load_dat.h
@@ -0,0 +1,11 @@
+#pragma once
+
+extern unsigned char* gSoundDataADSR;
+
+extern unsigned char* gSoundDataRaw;
+
+extern unsigned char* gMusicData;
+
+#ifndef VERSION_SH
+extern unsigned char* gBankSetsData;
+#endif
