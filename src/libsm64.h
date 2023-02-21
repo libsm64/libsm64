d55daf4319fda5bc07176ef2d2323069cc9d0870
Renato Rotenerg <renato.rotenberg@gmail.com>
2023-02-20 21:53:30 -0300

    Add some more mario functions
    sm64_set_mario_invincibility
    sm64_set_mario_health
    sm64_mario_extend_cap

--- src/libsm64.h
+++ src/libsm64.h
@@ -160,12 +160,15 @@ extern SM64_LIB_FN void sm64_set_mario_angle(int32_t marioId, float x, float y, 
 extern SM64_LIB_FN void sm64_set_mario_faceangle(int32_t marioId, float y);
 extern SM64_LIB_FN void sm64_set_mario_velocity(int32_t marioId, float x, float y, float z);
 extern SM64_LIB_FN void sm64_set_mario_forward_velocity(int32_t marioId, float vel);
+extern SM64_LIB_FN void sm64_set_mario_invincibility(int32_t marioId, int16_t timer);
 extern SM64_LIB_FN void sm64_set_mario_water_level(int32_t marioId, signed int level);
 extern SM64_LIB_FN void sm64_set_mario_gas_level(int32_t marioId, signed int level);
+extern SM64_LIB_FN void sm64_set_mario_health(int32_t marioId, uint16_t health);
 extern SM64_LIB_FN void sm64_mario_take_damage(int32_t marioId, uint32_t damage, uint32_t subtype, float x, float y, float z);
 extern SM64_LIB_FN void sm64_mario_heal(int32_t marioId, uint8_t healCounter);
 extern SM64_LIB_FN void sm64_mario_kill(int32_t marioId);
 extern SM64_LIB_FN void sm64_mario_interact_cap(int32_t marioId, uint32_t capFlag, uint16_t capTime, uint8_t playMusic);
+extern SM64_LIB_FN void sm64_mario_extend_cap(int32_t marioId, uint16_t capTime);
 extern SM64_LIB_FN bool sm64_mario_attack(int32_t marioId, float x, float y, float z, float hitboxHeight);
 
 extern SM64_LIB_FN uint32_t sm64_surface_object_create( const struct SM64SurfaceObject *surfaceObject );
